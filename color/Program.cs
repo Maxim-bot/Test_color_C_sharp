40e204ea69a0d7f1b5ec0cfdc97654ec9881e3d1
Макс E <max0681832239@hotmail.com>
2021-08-29 12:58:21 +0300

    Test

---
+++ color/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace color
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("каждый охотник желает знать где сидит фазан");
+
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.Write("");
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.Write("");
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.Write("");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write("");
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.Write("");
+            Console.ForegroundColor = ConsoleColor.DarkBlue;
+            Console.Write("");
+            Console.ForegroundColor = ConsoleColor.DarkMagenta;
+            Console.Write("");
+            Console.ResetColor();
+
+        }
+    }
+}
